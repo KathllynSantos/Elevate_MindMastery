e8c1ffd91231038e9634764555fa8ad6fdba995e
KathllynSantos <kathllyn.leticia@gmail.com>
2025-01-10 22:05:56 -0300

    Selection de comentarios

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const menuToggle = document.getElementById("menu-toggle");
     menuToggle.addEventListener("click", () => {
       menu.classList.toggle("active");
     });
+    document.addEventListener('DOMContentLoaded', () => {
+        const prevButton = document.querySelector('.prev');
+        const nextButton = document.querySelector('.next');
+        const carouselContainer = document.querySelector('.carousel-container');
+      
+        // Define o tamanho do scroll (igual à largura de um card)
+        const scrollAmount = carouselContainer.offsetWidth / 3;
+      
+        // Evento para a seta anterior
+        prevButton.addEventListener('click', () => {
+          carouselContainer.scrollLeft -= scrollAmount;
+        });
+      
+        // Evento para a seta próxima
+        nextButton.addEventListener('click', () => {
+          carouselContainer.scrollLeft += scrollAmount;
+        });
+      });
+      
